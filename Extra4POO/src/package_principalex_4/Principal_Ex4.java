690257845b3c80780440a02a5dfb653133e2ba6a
Roberto Santopietro <santoplis@yahoo.com>
2022-08-12 19:51:52 -0300

    incorporo extra 4 y 5

---
+++ Extra4POO/src/package_principalex_4/Principal_Ex4.java
@@ -0,0 +1,23 @@
+
+package package_principalex_4;
+
+import Servicio_Nif.Clase_Servicio;
+
+
+public class Principal_Ex4 {
+
+    
+    public static void main(String[] args) {
+        
+        Clase_Servicio s1 = new Clase_Servicio();
+        Clase_Servicio s2 = new Clase_Servicio();
+
+        s1.crearNif();
+
+        s1.mostrar();
+
+        s2.crearNif();
+        s2.mostrar();
+      }
+    
+}
---
+++ Extra5POO/src/extra5/Extra5.java
@@ -0,0 +1,15 @@
+
+package extra5;
+
+import newpackage.mesSecreto;
+
+public class Extra5 {
+
+  
+    public static void main(String[] args) {
+      mesSecreto m1 =new mesSecreto();
+      
+      m1.adivinarMes();
+    }
+    
+}
